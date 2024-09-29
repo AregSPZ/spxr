6e4962ae9dc5c0325b44b48c3aaa2e08d5a4ab59
Areg Sargsyan <quant0027@gmail.com>
2024-09-29 10:11:27 +0400

    0.1.2.

--- spxr/ml.py
+++ spxr/ml.py
@@ -1,41 +1,47 @@
 import numpy as np
-from sklearn.linear_model import LinearRegression, ElasticNet, SGDRegressor, LogisticRegression, SGDClassifier, ElasticNet 
-from sklearn.tree import DecisionTreeRegressor
-from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor, ExtraTreesRegressor, HistGradientBoostingRegressor, RandomForestClassifier, ExtraTreesClassifier, GradientBoostingClassifier 
+from sklearn.linear_model import LinearRegression, ElasticNet, SGDRegressor, LogisticRegression, SGDClassifier, ElasticNet, Ridge, Lasso, RidgeClassifier, RidgeCV, LassoCV, LogisticRegressionCV, ElasticNetCV, RidgeClassifierCV
+from sklearn.tree import DecisionTreeRegressor, DecisionTreeClassifier
+from sklearn.ensemble import RandomForestRegressor, GradientBoostingRegressor, AdaBoostRegressor, ExtraTreesRegressor, HistGradientBoostingRegressor, RandomForestClassifier, ExtraTreesClassifier, GradientBoostingClassifier, AdaBoostClassifier, HistGradientBoostingClassifier 
 from xgboost import XGBRegressor, XGBClassifier
 from sklearn.svm import LinearSVC, SVC, LinearSVR, SVR
 from sklearn.model_selection import cross_val_predict
-from sklearn.metrics import mean_squared_error, f1_score, precision_score, recall_score, classification_report, confusion_matrix, accuracy_score, roc_auc_score, auc, precision_recall_curve, average_precision_score, roc_auc_score
+from sklearn.metrics import mean_squared_error, f1_score, precision_score, recall_score, classification_report, confusion_matrix, accuracy_score, roc_auc_score, precision_recall_curve, auc
+from sklearn.preprocessing import make_pipeline, PolynomialFeatures
 
 
 
 
-def compare_classifiers(X, y, metrics, svm=False):
+
+def compare_classifiers(X, y, models=[
+    LogisticRegression(random_state=42),
+    RidgeClassifier(random_state=42),
+    SGDClassifier(random_state=42),
+    XGBClassifier(random_state=42),
+    DecisionTreeClassifier(random_state=42),
+    ExtraTreesClassifier(random_state=42),
+    RandomForestClassifier(random_state=42),
+    AdaBoostClassifier(random_state=42),
+    GradientBoostingClassifier(random_state=42),
+    ], metrics=['classification_report'], cv=3):
+
     """Compare the performance of the most prominent Scikit-learn classifiers using cross_val_predict and specified metrics
     X: features
     y: target variable
-    metrics: list of metrics to evaluate the classifiers
-    svm: boolean, whether to include SVM classifiers"""
+    models: list of classifiers to evaluate
+    metrics: list of metrics to evaluate the classifiers"""
 
-    classifiers = [
-    LogisticRegression(random_state=42),
-    SGDClassifier(random_state=42),
-    RandomForestClassifier(random_state=42),
-    ExtraTreesClassifier(random_state=42),
-    GradientBoostingClassifier(random_state=42),
-    XGBClassifier(random_state=42)
-    ]
 
-    if svm: 
-        classifiers.append(LinearSVC())
-        classifiers.append(SVC(kernel='linear'))
-        classifiers.append(SVC(kernel='poly'))
-        classifiers.append(SVC(kernel='rbf'))   
-
-    for model in classifiers:
+    for model in models:
         model_name = repr(model)
-        y_pred = cross_val_predict(model, X, y, cv=3) # get predictions using cross_val_predict
+        y_pred = cross_val_predict(model, X, y, cv=cv) # get predictions using cross_val_predict
         results = {metric.__name__: metric(y, y_pred) for metric in metrics}
+        
+        # Calculate precision-recall AUC if specified
+        if 'pr_auc_score' in [metric.__name__ for metric in metrics]:
+            precision, recall, _ = precision_recall_curve(y, y_pred)
+            pr_auc_score = auc(recall, precision)
+            results['pr_auc_score'] = pr_auc_score
+        
         results_str = ', '.join([f'{name}={score}' for name, score in results.items()]) # format the results
         print(f'{model_name}: {results_str}')
 
@@ -44,31 +50,27 @@ def rmse(y_true, y_pred): # the metric we will use to evaluate the regressors
     return np.sqrt(mean_squared_error(y_true, y_pred))
 
 
-def compare_regressors(X, y, svm = False):
-    """Evaluate the performance of the most prominent Scikit-learn regressors using cross_val_predict and RMSE"""
-
-    regressors = [
-    LinearRegression(),
+def compare_regressors(X, y, models=
+    [LinearRegression(),
+    Ridge(),
     ElasticNet(),
     SGDRegressor(random_state=42),
+    XGBRegressor(random_state=42),
     DecisionTreeRegressor(random_state=42),
-    RandomForestRegressor(random_state=42),
     ExtraTreesRegressor(random_state=42),
-    HistGradientBoostingRegressor(random_state=42),
-    GradientBoostingRegressor(random_state=42),
+    RandomForestRegressor(random_state=42),
     AdaBoostRegressor(random_state=42),
-    XGBRegressor(random_state=42)
-    ]
+    GradientBoostingRegressor(random_state=42),
+    ],
+    cv=3):
 
-    if svm: 
-        regressors.append(LinearSVR())
-        regressors.append(SVR(kernel='linear'))
-        regressors.append(SVR(kernel='poly'))
-        regressors.append(SVR(kernel='rbf'))
+    """Evaluate the performance of the most prominent Scikit-learn regressors using cross_val_predict and RMSE
+    X: features
+    y: target variable
+    models: list of regressors to evaluate"""
 
-
-    for model in regressors:
+    for model in models:
         model_name = repr(model)
-        y_pred = cross_val_predict(model, X, y, cv=3)
+        y_pred = cross_val_predict(model, X, y, cv=cv)
         score = rmse(y, y_pred)
         print(f'{model_name}: RMSE = {score}')
